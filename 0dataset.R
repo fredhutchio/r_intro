b2a8109af8945d16c054551182561fccd6bf89ff
k8hertweck <k8hertweck@gmail.com>
2018-07-24 17:10:08 -0700

    adding more cancer types

--- 0dataset.R
+++ 0dataset.R
@@ -11,23 +11,60 @@ library(tidyverse)
 
 # view data available for lung cancer
 TCGAbiolinks:::getProjectSummary("TCGA-LUSC") # lung cancer
-TCGAbiolinks:::getProjectSummary("TCGA-BRCA") # breast cancer
-TCGAbiolinks:::getProjectSummary("TCGA-PRAD") # prostate cancer
 # download and parse clinical data 
 LUSC <- GDCquery_clinic(project = "TCGA-LUSC", type = "clinical")
+BLCA <- GDCquery_clinic(project = "TCGA-BLCA", type = "clinical")
 BRCA <- GDCquery_clinic(project = "TCGA-BRCA", type = "clinical")
+CESC <- GDCquery_clinic(project = "TCGA-CESC", type = "clinical")
+COAD <- GDCquery_clinic(project = "TCGA-COAD", type = "clinical")
+GBM <- GDCquery_clinic(project = "TCGA-GBM", type = "clinical")
+LGG <- GDCquery_clinic(project = "TCGA-LGG", type = "clinical")
+MESO <- GDCquery_clinic(project = "TCGA-MESO", type = "clinical")
 PRAD <- GDCquery_clinic(project = "TCGA-PRAD", type = "clinical")
+READ <- GDCquery_clinic(project = "TCGA-READ", type = "clinical")
+SKCM <- GDCquery_clinic(project = "TCGA-SKCM", type = "clinical")
+STAD <- GDCquery_clinic(project = "TCGA-STAD", type = "clinical")
+THYM <- GDCquery_clinic(project = "TCGA-THYM", type = "clinical")
+UCEC <- GDCquery_clinic(project = "TCGA-UCEC", type = "clinical")
+UCS <- GDCquery_clinic(project = "TCGA-UCS", type = "clinical")
 
 # save raw data
 write.csv(LUSC, "extra/LUSC.csv", row.names = FALSE)
+write.csv(BLCA, "extra/BLCA.csv", row.names = FALSE)
 write.csv(BRCA, "extra/BRCA.csv", row.names = FALSE)
+write.csv(CESC, "extra/CESC.csv", row.names = FALSE)
+write.csv(COAD, "extra/COAD.csv", row.names = FALSE)
+write.csv(GBM, "extra/GBM.csv", row.names = FALSE)
+write.csv(LGG, "extra/LGG.csv", row.names = FALSE)
+write.csv(MESO, "extra/MESO.csv", row.names = FALSE)
 write.csv(PRAD, "extra/PRAD.csv", row.names = FALSE)
+write.csv(READ, "extra/READ.csv", row.names = FALSE)
+write.csv(SKCM, "extra/SKCM.csv", row.names = FALSE)
+write.csv(STAD, "extra/STAD.csv", row.names = FALSE)
+write.csv(THYM, "extra/THYM.csv", row.names = FALSE)
+write.csv(UCEC, "extra/UCEC.csv", row.names = FALSE)
+write.csv(UCS, "extra/UCS.csv", row.names = FALSE)
 # load raw data
 LUSC <- read.csv("extra/LUSC.csv")
+BLCA <- read.csv("extra/BLCA.csv")
 BRCA <- read.csv("extra/BRCA.csv")
+CESC <- read.csv("extra/CESC.csv")
+COAD <- read.csv("extra/COAD.csv")
+GBM <- read.csv("extra/GBM.csv")
+LGG <- read.csv("extra/LGG.csv")
+MESO <- read.csv("extra/MESO.csv")
 PRAD <- read.csv("extra/PRAD.csv")
+READ <- read.csv("extra/READ.csv")
+SKCM <- read.csv("extra/SKCM.csv")
+STAD <- read.csv("extra/STAD.csv")
+THYM <- read.csv("extra/THYM.csv")
+UCEC <- read.csv("extra/UCEC.csv")
+UCS <- read.csv("extra/UCS.csv")
 
 #### Clean and save final data ####
+
+# bind all together
+all_cancer <- rbind(LUSC, BLCA, BRCA, CESC, COAD, GBM, LGG, LUSC, MESO, PRAD, READ, SKCM, STAD, THYM, UCEC, UCS)
 
 # inspecting variables 
 str(LUSC)
@@ -42,9 +79,6 @@ LUSC <- select(LUSC, -empty)
 # identify columns of interest
 good_col <- colnames(LUSC)
 # extract columns of interest from other cancers
-BRCA <- select(BRCA, good_col)
-PRAD <- select(PRAD, good_col)
-# bind all together
-all_cancer <- rbind(LUSC, BRCA, PRAD)
+all_cancer_good <- select(all_cancer, good_col)
 # save final dataset
-write.csv(all_cancer, "extra/clinical.csv", row.names = FALSE)
+write.csv(all_cancer_good, "extra/clinical.csv", row.names = FALSE)
