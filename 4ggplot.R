5a3ba13218fd2b34e48998417627c9507f0727a3
k8hertweck <k8hertweck@gmail.com>
2018-07-26 16:14:46 -0700

    adding rest of plotting content

--- 4ggplot.R
+++ 4ggplot.R
@@ -61,7 +61,7 @@ smoke_plot +
        y="cigarettes per day") +
   theme_bw()
 
-## Challenge:
+## Challenge: create a scatterplot showing age at diagnosis vs years smoked with points colored by gender
 
 # save plot to file
 ggsave("figures/awesomePlot.jpg")
@@ -79,7 +79,7 @@ ggplot(data=smoke_complete,
   geom_boxplot() +
   geom_jitter(alpha = 0.3, color = "blue")
 
-# Challenge: violin plot
+# Challenge: visualize the same data as a violin plot in a color of your choice
 
 #### Plotting time series data (line plots) ####
 
@@ -104,7 +104,7 @@ ggplot(data=yearly_counts,
            group=disease, color = disease)) +
   geom_line()
 
-## Challenge:
+## Challenge: create a plot of birth year with two lines representing the number of patients of each gender 
 
 #### Faceting ####
 
@@ -141,4 +141,103 @@ ggplot(data=yearly_vital_counts,
   theme(panel.grid = element_blank())
 
 # facet in one column and variables in rows
+yearly_vital_birth <- birth_reduced %>%
+  group_by(year_of_birth, vital_status, disease) %>%
+  summarize(avg_age = mean(age_at_diagnosis))
+ggplot(data = yearly_vital_birth, 
+       aes(x = year_of_birth, y = avg_age, color = disease)) +
+  geom_line() +
+  facet_grid(vital_status ~ .)       
 
+# facet in one row and variables in column
+ggplot(data = yearly_vital_birth, 
+       aes(x = year_of_birth, y = avg_age, color = disease)) +
+  geom_line() +
+  facet_grid(. ~ vital_status)  
+
+#### Customization ####
+
+# changing axes
+ggplot(data=yearly_vital_counts, 
+       aes(x=year_of_birth, y=n, color = vital_status)) +
+  geom_line() +
+  facet_wrap(~ disease) + 
+  labs(title = "Vital status by year of birth",
+       x = "year of birth", y = " number of patients") +
+  theme_bw() 
+
+# increase font size
+ggplot(data=yearly_vital_counts, 
+       aes(x=year_of_birth, y=n, color = vital_status)) +
+  geom_line() +
+  facet_wrap(~ disease) + 
+  labs(title = "Vital status by year of birth",
+       x = "year of birth", y = " number of patients") +
+  theme_bw() +
+  theme(text = element_text(size=16))
+
+# increase font size
+ggplot(data=yearly_vital_counts, 
+       aes(x=year_of_birth, y=n, color = vital_status)) +
+  geom_line() +
+  facet_wrap(~ disease) + 
+  labs(title = "Vital status by year of birth",
+       x = "year of birth", y = " number of patients") +
+  theme_bw() +
+  theme(text = element_text(size=16))
+
+# rotate font
+ggplot(data=yearly_vital_counts, 
+       aes(x=year_of_birth, y=n, color = vital_status)) +
+  geom_line() +
+  facet_wrap(~ disease) + 
+  labs(title = "Vital status by year of birth",
+       x = "year of birth", y = " number of patients") +
+  theme_bw() +
+  theme(axis.text.x = element_text(colour = "grey20", size = 12,
+                    angle = 90, hjust = 0.5, vjust = 0.5),
+        axis.text.y = element_text(colour = "grey20", size = 12),
+        text = element_text(size = 16))
+
+# save theme changes
+grey_theme <- theme(axis.text.x = element_text(colour = "grey20", 
+                        size = 12,angle = 90, hjust = 0.5, vjust = 0.5),
+                    axis.text.y = element_text(colour = "grey20", size = 12),
+                    text = element_text(size = 16))
+
+# apply theme changes
+ggplot(data=yearly_vital_counts, 
+       aes(x=year_of_birth, y=n, color = vital_status)) +
+  geom_line() +
+  facet_wrap(~ disease) + 
+  labs(title = "Vital status by year of birth",
+       x = "year of birth", y = " number of patients") +
+  theme_bw() +
+  grey_theme
+
+#### Arranging and exporting plots ####
+
+install.packages("gridExtra")
+library(gridExtra)
+
+cig_boxplot <- ggplot(data=smoke_complete,
+      aes(x=vital_status, y=cigarettes_per_day)) +
+  geom_boxplot(color="tomato")
+
+birth_count_plot <- ggplot(data=yearly_counts, 
+    aes(x=year_of_birth, y=n, 
+        group=disease, color = disease)) +
+  geom_line()
+
+grid.arrange(cig_boxplot, birth_count_plot, ncol = 2, widths = c(4, 6))
+
+# exporting complex figures
+my_plot <- ggplot(data=yearly_counts, 
+                  aes(x=year_of_birth, y=n, 
+                      group=disease, color = disease)) +
+  geom_line()
+ggsave("figures/birth_yearly.png", my_plot, width = 15, height = 10)
+
+# This also works for grid.arrange() plots
+combo_plot <- grid.arrange(cig_boxplot, birth_count_plot, ncol = 2, widths = c(4, 6))
+ggsave("figures/combo_plot.png", combo_plot, width = 10, dpi = 300)
