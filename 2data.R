bdb1eba87d7fdfc5502dce58988b58d9295bc136
k8hertweck <k8hertweck@gmail.com>
2018-08-14 13:04:30 -0700

    extra notes and challenge exercises

--- 2data.R
+++ 2data.R
@@ -1,26 +1,59 @@
 #### Intro to R: working with dataframes ####
 
+#### Before class ####
+
+# share url to hackmd
+# share Dropbox script to this week's content
+
 #### Reading in data ####
+
+# project organization
+# remind that folders and directories are synonymous
+# tie into the strength of RStudio projects
+# directories for: data, figures, intermediate or results
+# describe use of buttons to make new directories
 
 # making a directory
 dir.create("data")
 
+# overview of tidy data principles
+# appropriate composition of data into tables still isn't commonly taught!
+# columns: variables
+# rows: observations
+# one piece of info per cell
+# csv: comma separated values (other things besides commas can have separators too though)
+
 # download data from url
 download.file("https://raw.githubusercontent.com/fredhutchio/R_intro/master/extra/clinical.csv", "data/clinical.csv")
+# describe how to tell if you can use a URL to download data files
+# can't download using Dropbox URLs
+
 # reading in data and saving to object
 clinical <- read.csv("data/clinical.csv")
 # recall object
 clinical
 # view in tab
 View(clinical)
+# describe where data came from and give brief overview of metadata: TCGA clinical data from different cancer types
+# show script used to produce data
 # note data in tab can't be edited!
 # note missing data
+
+# reading in data files formatted differently
+?read.csv
+# read.csv is one of a family of commands to import data
+# explore other options for importing data
+
+## Challenge: download, inspect, and import the following data files (smaller set of data from same clinical set)
+# example1: https://raw.githubusercontent.com/fredhutchio/R_intro/master/extra/clinical.tsv
+# read.delim
+# example2: https://raw.githubusercontent.com/fredhutchio/R_intro/master/extra/clinical.txt
+# read.table(header=TRUE)
+# can also read into R straight from URL using read.csv, but better practice to download data file for reference
 
 #### Data frames ####
 
 # data frames: tabular/spreadsheet data
-# rows are observations
-# columns are variables
 # table where columns are vectors all of same length
 # columns contain single type of data (characters, integers, factors)
 
@@ -39,7 +72,7 @@ rownames(clinical) # row names (only numbers here)
 str(clinical) # structure of object
 summary(clinical) # summary statistics by column
 
-## Challenge: Using the output of str(), what is the class, how many rows/columns, how many types of cancer (disease)?
+## Challenge: What is the class, how many rows/columns, how many types of cancer (disease)?
 
 #### Subsetting data frames ####
 
@@ -67,15 +100,18 @@ clinical["tumor_stage"] # result is data.frame
 clinical[ , "tumor_stage"] # results in vector
 clinical[["tumor_stage"]] # results in vector
 clinical$tumor_stage # results in vector
+# reference exploring different types of subsetting: https://davetang.org/muse/2013/08/16/double-square-brackets-in-r/
 
 ## Challenge: code as many different ways possible to extract the column days_to_death
+
+## Challenge: extract the first 6 rows for only age at diagnosis and days to death
 
 # performing functions on columns
 mean(clinical$days_to_death)
 # accomodate missing data
 mean(clinical$days_to_death, na.rm = TRUE)
 
-## Challenge: extract cigarettes per day into new object and calculcate the range and mean
+## Challenge: calculcate the range and mean for cigarettes per day
 
 #### Factors ####
 
@@ -113,3 +149,4 @@ clinical$race <- race
 plot(clinical$race)
 
 ## Challenge: replace "not reported" in ethnicity and race with NA
+# do a Google search to find additional strategies for renaming missing data
