0b4ed6f41bee41bee43c5c1d4150b29ccf4683cb
k8hertweck <k8hertweck@gmail.com>
2018-09-11 12:08:37 -0700

    adding a few exercises

--- exercises/2data_exercises.R
+++ exercises/2data_exercises.R
@@ -7,3 +7,9 @@
 #   working with data frames
 #   subsetting data frames
 #   manipulating factors
+
+# Using the same dataset as described in class, write code to accomplish the following tasks
+
+# extract the last 100 rows for only disease and race and save to an object called disease_race 
+
+# calculcate the range and mean for days to death
