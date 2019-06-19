d75b27f8d3664c307e185bb768c92091c82aaa03
Kate Hertweck <k8hertweck@gmail.com>
2019-06-19 16:14:45 -0700

    adding extra notes

--- week3.R
+++ week3.R
@@ -6,17 +6,19 @@
 
 # Today:
 # installing and loading packages
-# selecting columns and rows
+# selecting columns and rows (the tidyverse way)
 # combining commands using pipes
 # creating new columns by modifying existing data
 # summarizing data based on data in other columns
 
 #### Packages and tidyverse ####
 
-# start package installation quickly, as sometimes it takes a few minutes
+# start package installation at the start of class, as sometimes it takes a few minutes
 # install package (only once on your own computer)
 install.packages("tidyverse")
-# explain output in console; red doesn't necessarily mean anything is wrong!
+# explain output in console:
+#   red doesn't necessarily mean anything is wrong! Installing software gives a lot of output about things downloading and compiling
+#   if prompted, install all packages (say yes or all)
 
 # packages are collections of functions
 #   contributed by community, for public use
@@ -31,6 +33,7 @@ install.packages("tidyverse")
 # load library/package (needs to happen every time R restarts)
 library(tidyverse)
 # attaching packages references everything included in tidyverse
+# we need to load libraries the same way that we need to open software applications after installing them
 # there are many other packages included as dependencies
 # conflicts represents functions with same names are present in base R (stats)
 # double colon syntax (::) allows you to reference functions with same name,
@@ -44,7 +47,7 @@ library(tidyverse)
 
 #### Selecting columns and rows ####
 
-# create data directory and download data again, if needed
+# can probably skip this part: create data directory and download data again, if needed
 dir.create("data") # R will complain if this already exists
 download.file("https://raw.githubusercontent.com/fredhutchio/R_intro/master/extra/clinical.csv", "data/clinical.csv")
 
@@ -85,16 +88,17 @@ piped <- clinical %>%
 piped2 <- clinical %>%
   filter(year_of_birth < 1930) %>%
   select(race, ethnicity, disease)
-# does the order of commands differ?
+# does the order of commands differ? 
 piped3 <- clinical %>%
   select(race, ethnicity, disease) %>%
   filter(year_of_birth < 1930)
+# in this case, yes it does matter!
 
 ## Challenge: Use pipes to extract the columns gender, years_smoked, and year_of_birth from the object clinical for only living patients (vital_status) who have smoked fewer than 1 cigarettes per day
 
 #### Mutate ####
 
-# mutate allows unit conversions or ratios
+# mutate allows unit conversions or ratios, creates a new column
 # convert days to years
 clinical_years <- clinical %>%
   mutate(years_to_death = days_to_death / 365)
@@ -114,13 +118,13 @@ clinical %>%
 
 #### Split-apply-combine ####
 
-# frame the problem: we want to summarizing by gender
+# frame the problem: we want to summarize data by gender
 
 # show how summarize works
 summarize(clinical, mean_days_to_death = mean(days_to_death, na.rm = TRUE))
 
-# show table of categories in gender
-table(clinical$gender)
+# show categories in gender
+unique(clinical$gender)
 
 # group_by not always useful by itself, but powerful together with tally()
 # count number of individuals with each tumor stage
@@ -129,6 +133,7 @@ clinical %>%
   tally() # empty parentheses not required, but good practice
 # shows missing data, too
 
+# the split/apply/combine approach:
 # split data into groups,
 # apply an analysis to each group,
 # combine results back into one object
@@ -146,7 +151,7 @@ clinical %>%
   summarize(mean_days_to_death = mean(days_to_death))
 
 ## Challenge: create object called smoke_complete from clinical that contains no missing data for cigarettes per day or age at diagnosis 
-# Extra: how do you save resulting table to file?
+# Extra: how do you save resulting table to file? How would you find this answer?
 smoke_complete <- clinical %>%
   filter(!is.na(age_at_diagnosis)) %>%
   filter(!is.na(cigarettes_per_day))
